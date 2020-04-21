110387d6a795a4da3e628458447df48bf0ff1e21
zjun <503280155@qq.com>
2020-04-21 22:50:22 +0800

    202004212250

--- FlaskUploadsExample/apps/__init__.py
+++ FlaskUploadsExample/apps/__init__.py
@@ -7,15 +7,17 @@ from apps.utils import creat_folder
 app = Flask(__name__)
 app.debug = True
 
-app.config["SECRET_KEY"]="我有一个小秘密，就不告诉你"
+app.config["SECRET_KEY"] = "我有一个小秘密，就不告诉你"
 
-APPS_DiR = os.path.dirname(__file__)  # 得到_apps文件夹的目录     __file__ ：当前文件路径    即__init__文件所处的文件夹
-# print("__file__:",__file__)   #__file__: G:\Pyproject\FlaskServer\apps\__init__.py
+APPS_DiR = os.path.dirname(__file__)  # 得到当前脚本所在的文件路径即：apps文件夹的目录     __file__ ：当前文件路径    即__init__文件所处的文件夹
+
+# print("APPS_DiR",APPS_DiR)
+# print("__file__:",__file__)   # __file__: D:\Pyproject\FlaskServer\apps\__init__.py
 STATIC_DIR = os.path.join(APPS_DiR, 'static')  # 连接static文件夹目录
 
 app.config['UPLOAD_FOLDER'] = "uploads"  # 相对路径
 app.config['ABS_UPLOAD_FOLDER'] = os.path.join(STATIC_DIR, app.config['UPLOAD_FOLDER'])  # 绝对路径
-# print(app.config['ABS_UPLOAD_FOLDER'])    #:\Pyproject\FlaskServer\apps\static\uploads
+# print(app.config['ABS_UPLOAD_FOLDER'])    # :\Pyproject\FlaskServer\apps\static\uploads
 
 
 # 第一步：配置上传文件的保存地址
